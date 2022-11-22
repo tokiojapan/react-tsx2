4d22e8a8a0ce5a69d4da245c9d697933c093eb2a
tokiojapan <118701082+tokiojapan@users.noreply.github.com>
2022-11-22 20:15:06 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import "./styles.css";
+import { useState } from "react";
+import axios from "axios";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
